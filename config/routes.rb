899d4cd180312cf79ae9b69a28ff4632148a27cd
Aiandrox <aiandrox7@gmail.com>
2020-02-10 19:41:53 +0900

    [Fix] ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     collection do
       post :confirm
     end
-    patch 'confirm', to: :edit_confirm, on: :member, as: :confirm
+    patch 'confirm', to: 'posts#edit_confirm', on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
