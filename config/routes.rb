64994c82ae85385d792b8864b57d1620b50e70dc
Aiandrox <aiandrox7@gmail.com>
2020-02-10 20:05:39 +0900

    [Fix] collectionを一行で記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
   resources :albums
   resources :posts do
-    collection do
-      post :confirm
-    end
+    post :confirm, on: :collection
     patch 'confirm', to: 'posts#edit_confirm', on: :member
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
